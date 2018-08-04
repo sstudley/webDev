6144f0ba98fecc435b238fc23107f7a9a34530b2
Steve Studley <sstudley1@gmail.com>
2018-08-04 18:08:43 -0400

    notes add list started

--- OOP/app.js
+++ OOP/app.js
@@ -1,8 +1,22 @@
 console.log('starting app');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
 
-var user = os.userInfo();
+const notes = require('./notes.js');
 
-console.log(user);
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
+
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading note');
+} else if (command === 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Command not recognized');
+}
