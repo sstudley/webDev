ebf0d05e97638c598dcbe555bb08479e46f33a79
Steve Studley <sstudley1@gmail.com>
2018-08-05 13:27:28 -0400

    add base routines add list read remove

--- OOP/notes.js
+++ OOP/notes.js
@@ -1,6 +1,25 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
-}
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Reading note titled: ', title );
+};
+
+var removeNote = (title) => {
+  console.log('Removing note titled: ', title );
+};
+
+
+module.exports = { 
+  addNote, // addNote: addNote ES5
+  getAll,
+  getNote,
+  removeNote
+};
